140aa69a79d63b7330cbd1cb57d375f0852e6452
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2018-06-21 18:34:57 +0200

    sidebar.js :: keep only required files;; rest js files are deleted from the project

--- UserInterface/master_thesis_user_interface/src/App.js
+++ UserInterface/master_thesis_user_interface/src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Components/sidebar';
-import DataTypeSelection from './Components/js/DataTypeSelection';
-import DataSetSelection from './Components/js/DataSetSelection';
 import AddNewDataset from './Components/js/AddNewDataset';
-import Visualization from './Components/js/Visualization';
-import Visualizationb from './Components/js/Visualizationb';
 import MultipleFormInput from './Components/js/MultipleFormInput';
 import EvaluationCriteriaSelection from './Components/js/EvaluationCriteriaSelection';
-import learningWebSocket from './Components/js/learningWebSocket'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -23,14 +18,9 @@ class App extends Component {
                   <Sidebar />
                 </div>
                 <div className="col-sm-10 col-md-9 col-lg-10" style={{'backgroundColor': 'lavenderblush'}}>
-                  <Route exact path="/DataTypeSelection" component={DataTypeSelection} />
-                  <Route exact path="/DataSetSelection" component={DataSetSelection} />
                   <Route exact path="/AddNewDataset" component={AddNewDataset} />
-                  <Route exact path="/Visualizationb" component={Visualizationb} />
-                  <Route exact path="/Visualization" component={Visualization} />
                   <Route exact path="/EvaluationCriteriaSelection" component={EvaluationCriteriaSelection} />
                   <Route exact path="/MultipleFormInput" component={MultipleFormInput} />
-                  <Route exact path="/learningWebSocket" component={learningWebSocket} />
                 </div>
             </div>
           </Router>
--- UserInterface/master_thesis_user_interface/src/Components/sidebar.js
+++ UserInterface/master_thesis_user_interface/src/Components/sidebar.js
@@ -7,15 +7,9 @@ class Sidebar extends Component{
     return (
       <div className="sideNav">
           <ul>
-            <li><Link to="/DataTypeSelection">Data Type Selection</Link></li>
-            <li><Link to="/DataSetSelection">DataSet Selection </Link></li>
             <li><Link to="/AddNewDataSet">Add New Dataset </Link></li>
-            <li><Link to="/Visualizationb">Visualizationb </Link></li>
-            <li><Link to="/Visualization">Visualization </Link></li>
-            <li><Link to="/EvaluationCriteriaSelection">Evaluation </Link></li>
             <li><Link to="/EvaluationCriteriaSelection">Evaluation </Link></li>
             <li><Link to="/MultipleFormInput">Multiple </Link></li>
-            <li><Link to="/learningWebSocket">Learning </Link></li>
           </ul>
       </div>
 
